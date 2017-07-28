14c242f50e0e022b6c954c96d955d7c42199ab0f
Jason <jasonjones@salesforce.com>
2017-07-28 11:37:28 -0700

    Add port to config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 export default {
     'development': {
+        'port': process.env.PORT || 3000,
         'dbUrl': 'mongodb://mongo/testdb'
     }
 }
--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
 import app from './config/app';
-import config from './config/config';
+import Config from './config/config';
 import db from './config/db';
 
 const env = process.env.NODE_ENV || "development";
+const config = Config[env];
 
-db(config[env]);
+db(config);
 
-app.listen(3000, () => {
-    console.log(`node server running on port 3000`);
+app.listen(config.port, () => {
+    console.log(`node server running on port ${config.port}`);
 });
