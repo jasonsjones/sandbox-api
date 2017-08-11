505de0ab3937b7c79d58dc36d0ab8e27f3d6246f
Jason <jasonjones@salesforce.com>
2017-08-11 08:24:48 -0700

    Refactor user routes to use controller methods

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 export default {
     'development': {
         'port': process.env.PORT || 3000,
+        'token_secret': 'gonavybeatarmy',
         'dbUrl': 'mongodb://mongo/testdb'
     }
 }
--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,153 +1,21 @@
 import multer from 'multer';
-import jwt from 'jsonwebtoken';
-import fs from 'fs';
-import User from './user.model';
-import Avatar from '../avatar/avatar.model';
+import * as UserController from './user.controller';
 
 export default (app) => {
 
     const upload = multer({dest: './uploads/'});
-    app.get('/api/users', (req, res) => {
-        User.find({}).exec()
-            .then(users => {
-                res.json({
-                    success: true,
-                    payload: users
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                res.json({
-                    success: false,
-                    message: 'error getting user'
-                });
-            });
-    });
 
-    app.get('/api/user/:id', (req, res) => {
-        User.findOne({_id: req.params.id}).exec()
-            .then(user => {
-                res.json({
-                    success: true,
-                    payload: user
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                res.json({
-                    success: false,
-                    message: 'error getting user'
-                });
-            });
-    });
+    app.get('/api/users', UserController.getUsers);
 
-    app.delete('/api/user/:id', (req, res) => {
-        User.findOne({_id: req.params.id}).exec()
-            .then(user => {
-                return user.remove();
-            })
-            .then(user => {
-                res.json({
-                    success: true,
-                    message: 'user removed',
-                    payload: user
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                res.json({
-                    success: false,
-                    message: 'error removing user'
-                });
-            });
-    });
+    app.get('/api/user/:id', UserController.getUser);
 
-    app.post('/api/signup', (req, res) => {
-        let newUser = new User(req.body);
-        newUser.save()
-            .then(user => {
-                res.json({
-                    success: true,
-                    message: 'new user saved',
-                    payload: user
-                })
-            })
-            .catch(err => {
-                console.log(err);
-                res.json({
-                    success: false,
-                    message: 'error saving new user'
-                });
-            });
-    });
+    app.delete('/api/user/:id', UserController.deleteUser);
 
-    app.post('/api/user/:userid/avatar', authenticate, upload.single('avatar'), (req, res) => {
-        let userPromise = User.findOne({_id: req.params.userid}).exec();
+    app.post('/api/signup', UserController.signupUser);
 
-        let avatarPromise = userPromise.then(user => {
-            let avatar = new Avatar();
-            avatar.fileName = req.file.originalname;
-            avatar.contentType = req.file.mimetype;
-            avatar.fileSize = req.file.size / 1000;
-            avatar.defaultImg = false;
-            avatar.data = fs.readFileSync(req.file.path);
-            avatar.user = user._id;
-            return avatar.save();
-        });
+    app.post('/api/user/:userid/avatar', authenticate, upload.single('avatar'), UserController.uploadUserAvatar);
 
-        Promise.all([userPromise, avatarPromise]).then(values => {
-            let [user, img] = values;
-            fs.unlinkSync(req.file.path);
-            user.avatar = img._id;
-            user.avatarUrl = `http://localhost:3000/api/avatar/${img._id}`;
-            return user.save();
-        }).then(user => {
-            res.json({
-                success: true,
-                message: 'avatar uploaded and saved',
-                payload: user
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.json({
-                success: false,
-                message: 'error uploading avatar'
-            });
-        });
-    });
-
-    app.post('/api/login', (req, res) => {
-        User.findOne({email: req.body.email}).exec()
-            .then(user => {
-                if (user.verifyPassword(req.body.password)) {
-                    let token = jwt.sign(user._id, 'SECRET', {
-                        expiresIn: '24hr'
-                    });
-                    res.json({
-                        success: true,
-                        message: 'user authenticated',
-                        payload: {
-                            user,
-                            token
-                        }
-                    });
-                } else {
-                    res.json({
-                        success: false,
-                        message: 'invalid username or password',
-                        payload: null
-                    });
-                }
-            })
-            .catch(err => {
-                res.json({
-                    success: false,
-                    message: 'user not found',
-                    payload: null
-                });
-            });
-    });
+    app.post('/api/login', UserController.loginUser);
 
     function authenticate(req, res, next) {
         let token = req.body.token || req.headers['x-token'];
