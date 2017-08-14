9b16fe471a264beb9e78117b677fd651ca91513b
Jason <jasonjones@salesforce.com>
2017-08-14 08:32:34 -0700

    Refactor middleware fns to own module

--- src/user/user.model.js
+++ src/user/user.model.js
@@ -1,52 +1,21 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt-nodejs';
-import Avatar from '../avatar/avatar.model';
+import * as middleware from './user.model.middleware';
 const Schema = mongoose.Schema;
 
 const baseUrl = 'http://localhost:3000';
 
 const userSchema = new Schema({
-    name: String,
+    name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
-    password: String,
+    password: {type: String, required: true},
     avatar: {type: Schema.Types.ObjectId, ref: 'Avatar'},
     avatarUrl: {type: String, default: `${baseUrl}/api/avatar/default`}
 }, {timestamps: true});
 
-userSchema.pre('save', function (next) {
-    let user = this;
-    if (!user.isModified('password')) {
-        return next();
-    }
-    bcrypt.genSalt(10, (err, salt) => {
-        if (err) return next(err);
-        bcrypt.hash(user.password, salt, null, (err, hash) => {
-            if (err) return next(err);
-            user.password = hash;
-            next();
-        });
-    });
-});
-
-userSchema.post('save', function (error, user, next) {
-    if (error.name === 'MongoError' && error.code == 11000) {
-        next(new Error('There was a duplicate key error'))
-    } else {
-        next(err);
-    }
-});
-
-userSchema.post('remove', function (user) {
-    Avatar.findOne(user.avatar).exec()
-        .then(avatar => {
-            if (avatar && avatar.user.equals(user._id)) {
-                return avatar.remove();
-            }
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
+userSchema.pre('save', middleware.hashPassword);
+userSchema.post('save', middleware.checkForErrors);
+userSchema.post('remove', middleware.removeAvatarOnDelete);
 
 userSchema.methods.verifyPassword = function (password) {
     return bcrypt.compareSync(password, this.password);
---
+++ src/user/user.model.middleware.js
@@ -0,0 +1,37 @@
+import bcrypt from 'bcrypt-nodejs';
+import Avatar from '../avatar/avatar.model';
+
+export function hashPassword(next) {
+    let user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    bcrypt.genSalt(12, (err, salt) => {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, null, (err, hash) => {
+            if (err) return next(err);
+            user.password = hash;
+            next(null, user);
+        });
+    });
+}
+
+export function checkForErrors(err, user, next) {
+    if (err.name === 'MongoError' && err.code == 11000) {
+        next(new Error('There was a duplicate key error'))
+    } else {
+        next(err);
+    }
+}
+
+export function removeAvatarOnDelete(user) {
+    Avatar.findOne(user.avatar).exec()
+        .then(avatar => {
+            if (avatar && avatar.user.equals(user._id)) {
+                return avatar.remove();
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
