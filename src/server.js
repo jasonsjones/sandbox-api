06dad2a1c643fcc591ab0256519cc8382147c7a1
Jason <jasonjones@salesforce.com>
2017-08-18 11:43:53 -0700

    Add environment vars to docker-compose.yml

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
-const env = process.env.NODE_ENV || "development";
-
 import app from './config/app';
 import Config from './config/config';
 import db from './config/db';
 
+const env = process.env.NODE_ENV || "development";
 const config = Config[env];
 db(config);
 
