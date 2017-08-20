afd45d0f260f16a210fa1e46d2aa7ac2e28f12dd
Jason <jasonjones@salesforce.com>
2017-08-19 20:24:59 -0700

    Fix avatar model middleware

--- src/avatar/avatar.model.middleware.js
+++ src/avatar/avatar.model.middleware.js
@@ -3,9 +3,11 @@ import User from '../user/user.model';
 export function removeAvatarRefFromUser(avatar) {
     User.findById(avatar.user).exec()
         .then(user => {
-            user.avatar = null;
-            user.avatarUrl = 'http://localhost:3000/api/avatar/default';
-            return user.save();
+            if (user && user.avatar.equals(avatar._id)) {
+                user.avatar = null;
+                user.avatarUrl = 'http://localhost:3000/api/avatar/default';
+                return user.save();
+            }
         })
         .catch(err => {
             console.log(err);
