5651a7eaf168258e2666fbddf76a556bb94ada2b
Jason <jasonjones@salesforce.com>
2017-08-23 08:57:22 -0700

    Add config for test environment

--- src/config/config.js
+++ src/config/config.js
@@ -2,6 +2,11 @@ export default {
     'development': {
         'port': process.env.PORT || 3000,
         'token_secret': 'gonavybeatarmy',
+        'dbUrl': 'mongodb://mongo/sandboxapi'
+    },
+    'test': {
+        'port': process.env.PORT || 3000,
+        'token_secret': 'gonavybeatarmy',
         'dbUrl': 'mongodb://mongo/testdb'
     }
 }
