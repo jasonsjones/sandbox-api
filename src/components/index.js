828f8eb30ef90f1cdc9c2b3813ecf10913aa0d72
Jason <jasonjones@salesforce.com>
2017-09-05 07:35:32 -0700

    Add skeleton for react client

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,15 @@
-console.log('hello console from webpack');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = (props) => {
+    return (
+        <div>
+            <h1>Hello from React</h1>
+            <h2>{props.message}</h2>
+        </div>
+    );
+}
+ReactDOM.render(
+    <App message="here are the props..."/>,
+    document.getElementById('root')
+);
