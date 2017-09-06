15985b44fc41fda60c88b9c2dc617705b233b10a
Jason <jasonjones@salesforce.com>
2017-09-06 14:01:28 -0700

    Add SLDS package for styling

--- src/config/app.js
+++ src/config/app.js
@@ -8,7 +8,6 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(express.static('node_modules'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
