e6492d5e1bbd0fc01b98d3075a8f0b2a2eafb8f8
Jason <jasonjones@salesforce.com>
2017-09-07 13:16:20 -0700

    Add dotenv package

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default {
     'development': {
         'port': process.env.PORT || 3000,
-        'token_secret': 'gonavybeatarmy',
+        'token_secret': process.env.JWT_SECRET,
         'dbUrl': 'mongodb://mongo/sandboxapi'
     },
     'test': {
         'port': process.env.PORT || 3000,
-        'token_secret': 'gonavybeatarmy',
+        'token_secret': process.env.JWT_SECRET,
         'dbUrl': 'mongodb://mongo/testdb'
     }
 }
