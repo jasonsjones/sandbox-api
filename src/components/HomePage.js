1404e9b84383e59c618fc8617bca0cc38fc8a47b
Jason <jasonjones@salesforce.com>
2017-09-10 20:28:29 -0700

    Add prop validations to HomePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import * as authAction from '../actions/authActions';
 
@@ -13,14 +14,22 @@ class Home extends React.Component {
     }
 
     render() {
+        const { user } = this.props;
+
         return (
             <div className="slds-m-around_x-large">
                 <h1 className="slds-text-heading_large">Home page!</h1>
-                <h3 className="slds-text-heading_medium">Hello, {this.props.user.name}</h3>
+                <h3 className="slds-text-heading_medium">Hello, {user.name}</h3>
                 <button className="slds-button slds-button_neutral slds-m-top_medium" onClick={this.handleClick}>Logout</button>
             </div>
         );
     }
 }
+
+Home.propTypes = {
+    user: PropTypes.shape({
+        name: PropTypes.string.isRequired
+    })
+};
 
 export default Home;
