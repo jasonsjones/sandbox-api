2304714bbbd23fdc50a269d054ecf5e119694d48
Jason <jasonjones@salesforce.com>
2017-09-26 16:44:39 -0700

    Add session-secret prop to config

--- src/config/config.js
+++ src/config/config.js
@@ -5,11 +5,13 @@ export default {
     'development': {
         'port': process.env.PORT || 3000,
         'token_secret': process.env.JWT_SECRET,
+        'session-secret': process.env.SESSION_SECRET,
         'dbUrl': 'mongodb://mongo/sandboxapi'
     },
     'test': {
         'port': process.env.PORT || 3000,
         'token_secret': process.env.JWT_SECRET,
+        'session-secret': process.env.SESSION_SECRET,
         'dbUrl': 'mongodb://mongo/testdb'
     }
 }
