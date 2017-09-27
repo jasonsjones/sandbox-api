4b848973d49f937adec86bf1958e8627f7b5ffb9
Jason <jasonjones@salesforce.com>
2017-09-27 13:55:03 -0700

    Add cors and session to app

--- src/config/app.js
+++ src/config/app.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import session from 'express-session';
+import cors from 'cors';
+
+import Config from './config';
 import avatarRoute from '../api/avatar/avatar.routes';
 import userRoute from '../api/user/user.routes';
 import indexRoute from '../api/index/index.routes';
+
+const env = process.env.NODE_ENV || "development";
+const config = Config[env];
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,6 +20,13 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(morgan('dev'));
+app.use(cors());
+
+app.use(session({
+    secret: config.session_secret,
+    resave: false,
+    saveUninitialized: true
+}));
 
 avatarRoute(app);
 userRoute(app);
