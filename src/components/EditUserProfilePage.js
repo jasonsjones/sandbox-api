16d641fe2d7f8e0a4c3e59dd62c49e3ca58ad29a
Jason <jasonjones@salesforce.com>
2017-09-29 16:38:52 -0700

    Add skeleton for EditUserProfilePage cmp

---
+++ src/components/EditUserProfilePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class EditUserProfilePage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <h1>Edit User Profile Page</h1>
+        );
+    }
+
+}
+
+export default EditUserProfilePage;
