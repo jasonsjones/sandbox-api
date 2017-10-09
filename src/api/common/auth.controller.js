dc8b5813e15392b06e2807dd9fd61e1b8d7d20ea
Jason <jasonjones@salesforce.com>
2017-10-09 13:18:39 -0700

    Move sfdc auth/callback to auth controller

--- src/api/common/auth.controller.js
+++ src/api/common/auth.controller.js
@@ -1,4 +1,6 @@
 import jwt from 'jsonwebtoken';
+import fetch from 'node-fetch';
+
 import Config from '../../config/config';
 import User from '../user/user.model';
 
@@ -119,4 +121,40 @@ export const redirectToSFDC = (req, res) => {
                 '&redirect_uri=' + callback;
 
     res.redirect(url);
+}
+
+export const sfdcCallback = (req, res) => {
+    let access_token;
+    let clientId = process.env.SFDC_CLIENT_ID;
+    let clientSecret = process.env.SFDC_CLIENT_SECRET;
+    let callback = encodeURI('http://localhost:3000/auth/callback');
+    let code = req.query.code;
+    let bodyData = `grant_type=authorization_code&redirect_uri=${callback}&client_id=${clientId}&client_secret=${clientSecret}&code=${code}`;
+
+    fetch('https://login.salesforce.com/services/oauth2/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: bodyData
+    })
+    .then(res => res.json())
+    .then(json => {
+        access_token = json.access_token;
+        console.log(json);
+        return fetch(json.id, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${access_token}`
+            }
+        });
+    })
+    .then(res => res.json())
+    .then(user => {
+        console.log(user);
+        res.json({
+            success: true,
+            data: user
+        });
+    });
 }
--- src/api/common/auth.routes.js
+++ src/api/common/auth.routes.js
@@ -1,43 +1,8 @@
-import fetch from 'node-fetch';
 import * as authController from './auth.controller';
 
 export default (app) => {
 
     app.get('/auth/sfdc', authController.redirectToSFDC);
 
-    app.get('/auth/callback', (req, res) => {
-        let access_token;
-        let clientId = process.env.SFDC_CLIENT_ID;
-        let clientSecret = process.env.SFDC_CLIENT_SECRET;
-        let callback = encodeURI('http://localhost:3000/auth/callback');
-        let code = req.query.code;
-        let bodyData = `grant_type=authorization_code&redirect_uri=${callback}&client_id=${clientId}&client_secret=${clientSecret}&code=${code}`;
-
-        fetch('https://login.salesforce.com/services/oauth2/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            body: bodyData
-        })
-        .then(res => res.json())
-        .then(json => {
-            access_token = json.access_token;
-            console.log(json);
-            return fetch(json.id, {
-                method: 'GET',
-                headers: {
-                    'Authorization': `Bearer ${access_token}`
-                }
-            });
-        })
-        .then(res => res.json())
-        .then(user => {
-            console.log(user);
-            res.json({
-                success: true,
-                data: user
-            });
-        });
-    });
-};
+    app.get('/auth/callback', authController.sfdcCallback);
+}
