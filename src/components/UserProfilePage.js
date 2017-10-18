a10c90b7772fd206663087d1e5e12d31774cc335
Jason <jasonjones@salesforce.com>
2017-10-18 15:48:47 -0700

    Update styling

--- src/components/UserProfilePage.js
+++ src/components/UserProfilePage.js
@@ -4,6 +4,11 @@ import PropTypes from 'prop-types';
 
 import * as authAction from '../actions/authActions';
 
+const avatarStyles = {
+    borderRadius: "50%",
+    width: "200px"
+}
+
 class UserProfilePage extends React.Component {
     constructor(props) {
         super(props);
@@ -20,19 +25,23 @@ class UserProfilePage extends React.Component {
         return (
             <div className="slds-m-around_x-large container-content">
                 <h1 className="slds-text-heading_large slds-text-align_center">User Profile Page</h1>
-                <h3 className="slds-text-heading_medium">Hello, {user.name}</h3>
-                <p className="slds-text-body_regular slds-text-color_weak slds-m-bottom_medium">{user.email}</p>
-                <span className="slds-avatar slds-avatar_large slds-m-right_large">
-                    <img src={user.avatarUrl}/>
-                </span>
-                <button className="slds-button slds-button_neutral" onClick={this.handleClick}>Logout</button>
 
-                <div className="slds-m-top_large">
+                <div className="slds-grid slds-m-top_large">
+                    <div className="slds-m-bottom_large">
+                        <img style={avatarStyles} src={this.props.user.avatarUrl}/>
+                    </div>
+                    <div className="slds-grid slds-grid--vertical slds-m-left_large">
+                        <h3 className="slds-text-heading_medium">{user.name}</h3>
+                        <p className="slds-text-body_regular slds-text-color_weak slds-m-top_medium">{user.email}</p>
+                    </div>
+                </div>
+
+                <div className="slds-grid slds-grid_align-spread slds-m-top_large">
+                    <button className="slds-button slds-button_neutral" onClick={this.handleClick}>Logout</button>
                     <Link to="/editprofile">
                         <button className="slds-button slds-button_brand">Edit Profile</button>
                     </Link>
                 </div>
-
             </div>
         );
     }
